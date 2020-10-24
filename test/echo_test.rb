e0e8cd5f26a4981e2eb1bef8d849dcbbd2dfcf3e
Mark Cornelissen <mark@boxture.com>
2020-10-24 07:29:19 +0200

    Correct the test to actually work

--- test/echo_test.rb
+++ test/echo_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
-require 'pry-byebug'
 
 class EchoTest < CapybaraTestCase
   def test_echo
     visit '/'
     fill_in 'message', with: 'Hello, World!'
     click_on 'Submit'
-    assert page.has_content? 'Echo: Hello, World! 2'
+    assert page.has_content? 'Echo: Hello, World!'
   end
 end
