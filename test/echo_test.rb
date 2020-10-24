33b784d9850194d560628e72216785985bfad502
Mark Cornelissen <mark@boxture.com>
2020-10-24 11:49:57 +0200

    A test that passes and a test that fails

--- test/echo_test.rb
+++ test/echo_test.rb
@@ -1,10 +1,17 @@
 require 'test_helper'
 
 class EchoTest < CapybaraTestCase
-  def test_echo
+  def test_echo_that_passes
     visit '/'
     fill_in 'message', with: 'Hello, World!'
     click_on 'Submit'
     assert page.has_content? 'Echo: Hello, World!'
+  end
+
+  def test_echo_that_fails
+    visit '/'
+    fill_in 'message', with: 'Hello, World!'
+    click_on 'Submit'
+    assert page.has_content? 'Echo: Goodbye'
   end
 end
